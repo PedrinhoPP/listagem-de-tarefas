d95f23d84812cd18180c2adf357ee9fcca515e68
Pedro Augusto <124587051+PedrinhoPP@users.noreply.github.com>
2024-01-10 10:52:35 -0300

    Add files via upload
    Programa pra listar tarefas do dia dia

---
+++ script.js
@@ -0,0 +1,11 @@
+function addTask() {
+    const taskInput = document.getElementById('taskInput');
+    const taskList = document.getElementById('taskList');
+  
+    if (taskInput.value !== '') {
+      const newTask = document.createElement('li');
+      newTask.textContent = taskInput.value;
+      taskList.appendChild(newTask);
+      taskInput.value = '';
+    }
+  }
